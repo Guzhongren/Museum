e09e4daadfabc8424b416afe3e7a4546e942c7ac
Guzhongren <Guzhongren@live.cn>
2014-11-30 11:45:20 +0800

    提交

---
+++ WebApplication1/Script/MapScript.js
@@ -0,0 +1,17 @@
+﻿$(function () {
+    $("#Func1").click(function () {
+        $("#Tab1").slideToggle();
+        $("#Tab2").css("display", 'none');
+        $("#Tab3").css("display", 'none');
+    });
+    $("#Func2").click(function () {
+        $("#Tab2").slideToggle();
+        $("#Tab1").css("display", 'none');
+        $("#Tab3").css("display", 'none');
+    });
+    $("#Func3").click(function () {
+        $("#Tab3").slideToggle();
+        $("#Tab1").css("display", 'none');
+        $("#Tab2").css("display", 'none');
+    });
+});
