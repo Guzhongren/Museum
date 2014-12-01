d615e28ad776d2816717896d64180b5b4036eb4a
Guzhongren <Guzhongren@live.cn>
2014-12-01 19:21:21 +0800

    三层设计中

---
+++ ModalHelper/CulturalRelicModal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModalHelper
+{
+     public class CulturalRelicModal
+    {
+        public string imagePath { get; set; }
+        public string culturalRelicName { get; set; }
+        public string englishiName { get; set; }
+        public string cultrualRelicClassify { get; set; }
+        public string culturalRelicCode { get; set; }
+        public string culturalRelicMaterrial { get; set; }
+        public string culturalRelicPosition { get; set; }
+        public string culturalRelicBirthPlace { get; set; }
+        public string cultrualRelicBirthDataTime { get; set; }
+        public string culturalRelicDataofFigura { get; set; }
+        public string culturalRelicBrief { get; set; }
+    }
+}
---
+++ ModalHelper/SqlHelper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModalHelper
+{
+     public static class SqlHelper
+    {
+        private static readonly string configStr = ConfigurationManager.ConnectionStrings["connstr"].ToString();
+        public static int ExecuteNoQuery(string sql, params SqlParameter[] parameters)
+        {
+            using (SqlConnection conn = new SqlConnection(configStr))
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = sql;
+                    cmd.Parameters.AddRange(parameters);
+                    return cmd.ExecuteNonQuery();
+                }
+            }
+        }
+        public static object ExecutScale(string sql, params SqlParameter[] parameters)
+        {
+            using (SqlConnection conn = new SqlConnection(configStr))
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = sql;
+                    cmd.Parameters.AddRange(parameters);
+                    return cmd.ExecuteScalar();
+                }
+            }
+        }
+        public static DataTable ExecuteDataTabel(string sql, params SqlParameter[] parameters)
+        {
+            using (SqlConnection conn = new SqlConnection(configStr))
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = sql;
+                    cmd.Parameters.AddRange(parameters);
+                    SqlDataAdapter adater = new SqlDataAdapter(cmd);
+                    DataSet dataSet = new DataSet();
+                    adater.Fill(dataSet);
+                    return dataSet.Tables[0];
+                }
+            }
+        }
+    }
+}
