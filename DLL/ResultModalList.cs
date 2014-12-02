2ac567e03d033bd24007afd66e1f64a80c0f7c10
Guzhongren <guzhongren@live.cn>
2014-12-02 16:27:20 +0800

    Revert "完成DAL"
    This reverts commit 6bc0a39a81a92dbf58980f2fc21dd3964c2faaed.

--- DLL/ResultModalList.cs
+++ DLL/ResultModalList.cs
@@ -1,5 +1,4 @@
-﻿using ModalHelper;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
@@ -7,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DAL
+namespace DLL
 {
     public class ResultModalList
     {
@@ -21,29 +20,15 @@ namespace DAL
             int rowCount = dataTable.Rows.Count;
             if (rowCount <= 0)
             {
-                return null;
+                return list;
             }
             else
             {
-                for (int i = 0; i < rowCount; i++)
-                {
-                    CulturalRelicModal culturalRelicModal = new CulturalRelicModal();
-                    DataRow dataRow=dataTable.Rows[i];
-                    culturalRelicModal.imagePath = dataRow["imagePath"].ToString();
-                    culturalRelicModal.culturalRelicName = dataRow["culturalRelicName"].ToString();
-                    culturalRelicModal.englishiName = dataRow["englishiName"].ToString();
-                    culturalRelicModal.cultrualRelicClassify = dataRow["cultrualRelicClassify"].ToString();
-                    culturalRelicModal.culturalRelicCode = dataRow["culturalRelicCode"].ToString();
-                    culturalRelicModal.culturalRelicMaterrial = dataRow["culturalRelicMaterrial"].ToString();
-                    culturalRelicModal.culturalRelicPosition = dataRow["culturalRelicPosition"].ToString();
-                    culturalRelicModal.culturalRelicBirthPlace = dataRow["culturalRelicBirthPlace"].ToString();
-                    culturalRelicModal.cultrualRelicBirthDataTime = dataRow["cultrualRelicBirthDataTime"].ToString();
-                    culturalRelicModal.culturalRelicDataofFigura = dataRow["culturalRelicDataofFigura"].ToString();
-                    culturalRelicModal.culturalRelicBrief = dataRow["culturalRelicBrief"].ToString();
-                    list.Add(culturalRelicModal);
-                }
                 return list;
-            }   
+            }
+                
+
+            
         }
         
     }
--- ModalHelper/CulturalRelicModal.cs
+++ ModalHelper/CulturalRelicModal.cs
@@ -8,52 +8,16 @@ namespace ModalHelper
 {
      public class CulturalRelicModal
     {
-
-        #region 名称要和数据库里面的名称一致--->便于识别
-        /// <summary>
-        /// 图片路径
-        /// </summary>
         public string imagePath { get; set; }
-        /// <summary>
-        /// 名称
-        /// </summary>
         public string culturalRelicName { get; set; }
-        /// <summary>
-        /// EnlishName
-        /// </summary>
         public string englishiName { get; set; }
-        /// <summary>
-        /// 产状分类
-        /// </summary>
         public string cultrualRelicClassify { get; set; }
-        /// <summary>
-        /// 编码
-        /// </summary>
         public string culturalRelicCode { get; set; }
-        /// <summary>
-        /// 材质
-        /// </summary>
         public string culturalRelicMaterrial { get; set; }
-        /// <summary>
-        /// 展位
-        /// </summary>
         public string culturalRelicPosition { get; set; }
-        /// <summary>
-        ///产地
-        /// </summary>
         public string culturalRelicBirthPlace { get; set; }
-        /// <summary>
-        /// 制造日期
-        /// </summary>
         public string cultrualRelicBirthDataTime { get; set; }
-        /// <summary>
-        /// 出土日期
-        /// </summary>
         public string culturalRelicDataofFigura { get; set; }
-        /// <summary>
-        /// 简介
-        /// </summary>
-        public string culturalRelicBrief { get; set; } 
-        #endregion
+        public string culturalRelicBrief { get; set; }
     }
 }
